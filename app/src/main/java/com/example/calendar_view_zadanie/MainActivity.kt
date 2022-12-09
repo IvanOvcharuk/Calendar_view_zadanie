80a2f465852c2643067713e47f3415737d344c3b
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-12-09 14:57:51 +0100

    Pierwszy krok

--- app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calendar_view_zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var kalendarz = findViewById<CalendarView>(R.id.calendarView)
+        var wyjazd = findViewById<Button>(R.id.button)
+        var powrot = findViewById<Button>()
     }
 }
