22af56f0ae4874114dcfde2312a11ac16118b5ab
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-12-09 15:48:52 +0100

    Pierwszy drugi

--- app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var kalendarz = findViewById<CalendarView>(R.id.calendarView)
         var wyjazd = findViewById<Button>(R.id.button)
-        var powrot = findViewById<Button>()
+        var powrot = findViewById<Button>(R.id.button2)
+        var wyjazdview = findViewById<TextView>(R.id.textView)
+        var powrotview = findViewById<TextView>(R.id.textView2)
+
+        kalendarz.minDate = System.currentTimeMillis()
+        kalendarz.maxDate = System.currentTimeMillis() + (kalendarz.maxDate - System.currentTimeMillis())
+
+
+
     }
 }
