04500b476ebf2bf78ba74d7de5b30fe563874d80
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-12-09 16:53:15 +0100

    Trzeci krok ilosc dni

--- app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
+import java.text.DateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +17,40 @@ class MainActivity : AppCompatActivity() {
         var powrot = findViewById<Button>(R.id.button2)
         var wyjazdview = findViewById<TextView>(R.id.textView)
         var powrotview = findViewById<TextView>(R.id.textView2)
+        var policz = findViewById<TextView>(R.id.button4)
+        var policzview = findViewById<TextView>(R.id.textView3)
 
         kalendarz.minDate = System.currentTimeMillis()
-        kalendarz.maxDate = System.currentTimeMillis() + (kalendarz.maxDate - System.currentTimeMillis())
+        kalendarz.maxDate = System.currentTimeMillis(720)
 
+        var data_odjazdu = mutableListOf<Int>(0,0,0)
+        var data_powrotu= mutableListOf<Int>(0,0,0)
+        var data = arrayListOf<Int>(0,0,0)
+        kalendarz.setOnDateChangeListener() { CalendarView , i, i2, i3 ->
+            data[0] = i
+            data[1] = i2+1
+            data[2] = i3
+
+        }
+
+        wyjazd.setOnClickListener {
+            data_odjazdu[0] = data[0]
+            data_odjazdu[1] = data[1]
+            data_odjazdu[2] = data[2]
+            wyjazdview.text = "Data wyjazdu:  "+data[0].toString()+"-"+data[1].toString()+"-"+data[2].toString();
+        }
+        powrot.setOnClickListener {
+            data_powrotu[0] = data[0]
+            data_powrotu[1] = data[1]
+            data_powrotu[2] = data[2]
+            powrotview.text = "Data powrotu:  "+data[0].toString()+"-"+data[1].toString()+"-"+data[2].toString();
+        }
+        policz.setOnClickListener {Int
+            var temp1 = (data_powrotu[0]*360) + (data_powrotu[1]*30) + data_powrotu[2]
+            var temp2 = (data_odjazdu[0]*360) + (data_odjazdu[1]*30) + data_odjazdu[2]
+            var temp = temp1.toChar() - temp2.toChar()
+            policzview.text = "Ilosc dni: "+temp.toString()
+        }
 
 
     }
