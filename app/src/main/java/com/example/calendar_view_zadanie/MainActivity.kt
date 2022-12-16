4a161bc3c1da280849a39fbd8cba2384df4a3ef2
IvanOvcharuk <ivanovcharuk228@gmail.com>
2022-12-16 17:56:15 +0100

    Gotowe zadanie

--- app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/calendar_view_zadanie/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         var powrot = findViewById<Button>(R.id.button2)
         var wyjazdview = findViewById<TextView>(R.id.textView)
         var powrotview = findViewById<TextView>(R.id.textView2)
-        var policz = findViewById<TextView>(R.id.button4)
         var policzview = findViewById<TextView>(R.id.textView3)
 
         kalendarz.minDate = System.currentTimeMillis()
@@ -32,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         var data_odjazdu = mutableListOf<Int>(0,0,0)
         var data_powrotu= mutableListOf<Int>(0,0,0)
-        var data = arrayListOf<Int>(MilliToDate(kalendarz.date)[0],MilliToDate(kalendarz.date)[1],MilliToDate(kalendarz.date)[2])
+        var data = arrayListOf<Int>(czas(kalendarz.date)[0],czas(kalendarz.date)[1],czas(kalendarz.date)[2])
         kalendarz.setOnDateChangeListener() { CalendarView , i, i2, i3 ->
             data[0] = i
             data[1] = i2+1
@@ -44,30 +43,24 @@ class MainActivity : AppCompatActivity() {
             data_odjazdu[0] = data[0]
             data_odjazdu[1] = data[1]
             data_odjazdu[2] = data[2]
-            wyjazdview.text = "Data wyjazdu:  "+data[0].toString()+"-"+data[1].toString()+"-"+data[2].toString();
+            wyjazdview.text = "Data wyjazdu:  "+data[0].toString()+"/"+data[1].toString()+"/"+data[2].toString();
         }
         powrot.setOnClickListener {
             data_powrotu[0] = data[0]
             data_powrotu[1] = data[1]
             data_powrotu[2] = data[2]
-            powrotview.text = "Data powrotu:  "+data[0].toString()+"-"+data[1].toString()+"-"+data[2].toString();
+            powrotview.text = "Data powrotu:  "+data[0].toString()+"/"+data[1].toString()+"/"+data[2].toString();
 
-            // Jeśli obie daty nie są puste program wyświetli czas podróży w dniach
             if (data_odjazdu[0] != 0 && data_powrotu[0] != 0)
-                if(data_odjazdu[2] > data_powrotu[2]  && data_odjazdu[1] == data_powrotu[1])
+                if(data_odjazdu[0] > data_powrotu[0] || data_odjazdu[2] > data_powrotu[2]  && data_odjazdu[1] == data_powrotu[1])
                     policzview.text = "Nie możesz wyjechać później niż wrócisz!"
                 else
-                    nazwa(data_odjazdu, data_powrotu, policzview)
+                    policz(data_odjazdu, data_powrotu, policzview)
         }
-
-
-
-
-
     }
 }
 
-fun MilliToDate(milli : Long) : List<Int>
+fun czas(milli : Long) : List<Int>
 {
     val date = Date(milli)
     val formatter = SimpleDateFormat("yyyy/MM/dd")
@@ -76,8 +69,8 @@ fun MilliToDate(milli : Long) : List<Int>
     return ans;
 }
 
-fun nazwa(data_powrotu : MutableList<Int>, data_odjazdu : MutableList<Int>, policzview : TextView)
-{   var temp1 = (data_powrotu[0]*360) + (data_powrotu[1]*30) + data_powrotu[2]
-    var temp2 = (data_odjazdu[0]*360) + (data_odjazdu[1]*30) + data_odjazdu[2]
-    var temp = temp1.toChar() - temp2.toChar()
-    policzview.text = "Ilosc dni: " +temp.absoluteValue.toString()}
+fun policz(data_powrotu : MutableList<Int>, data_odjazdu : MutableList<Int>, policzview : TextView)
+{   var data1 = (data_powrotu[0]*360) + (data_powrotu[1]*30) + data_powrotu[2]
+    var data2 = (data_odjazdu[0]*360) + (data_odjazdu[1]*30) + data_odjazdu[2]
+    var datacalkowita = data1.toChar() - data2.toChar()
+    policzview.text = "Ilosc dni: " +datacalkowita.absoluteValue.toString()}
